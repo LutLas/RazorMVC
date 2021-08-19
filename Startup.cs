60744d336de3df446ae2247d7b90e4e745dfc733
Lutaka Chihota <lutakachihota@gmail.com>
2021-08-19 13:51:03 +0200

    Libman, Bootstrap

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,9 @@ namespace RazorMVC
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(it=> {
+                it.EnableEndpointRouting = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,13 +31,14 @@ namespace RazorMVC
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            /*app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
-            });
+            });*/
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
